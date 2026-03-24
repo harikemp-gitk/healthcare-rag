b7db665d828a6f098dd111353558319344a11e61
Vitthal <vitthal@Vitthals-MacBook-Pro.local>
2026-03-24 12:01:03 +0530

    Added Booking Agent

---
+++ graph/booking_graph.py
@@ -0,0 +1,29 @@
+"""
+This module defines the BookingGraph class, which is responsible for managing the graph of agents and tools related to booking tasks. It provides methods for adding agents and tools to the graph, as well as retrieving them based on their names or tags. The BookingGraph class serves as a central component for organizing and accessing the various agents and tools used in the booking process.
+"""
+from langgraph.graph import StateGraph,MessagesState, START
+from langgraph.prebuilt import ToolNode, tools_condition
+from agents.booking_agent import create_booking_node
+from tools.mcp_tools import get_mcp_client, get_calendar_tools
+
+async def build_booking_graph():
+    client = get_mcp_client()
+    calendar_tools  = await get_calendar_tools(client)
+    
+
+    if not calendar_tools:
+        raise RuntimeError("No calendar tools found in MCP server. Please add tools with the 'calendar' tag.")
+
+    booking_node, tools = create_booking_node(calendar_tools)
+
+    builder = StateGraph(MessagesState)
+
+    builder.add_node("booking_agent", booking_node)
+    builder.add_node("tools", ToolNode(tools))
+
+    builder.add_edge(START, "booking_agent")
+    builder.add_conditional_edges("booking_agent", tools_condition)
+    builder.add_edge("tools", "booking_agent")
+
+    booking_graph = builder.compile()
+    return booking_graph, client
---
+++ tools/mcp_tools.py
@@ -0,0 +1,31 @@
+import os
+import token
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+COMPOSIO_API_KEY = os.getenv("COMPOSIO_API_KEY" ,"")
+COMPOSIO_MCP_URL = os.getenv("COMPOSIO_MCP_URL", "")
+
+def get_mcp_client():
+    """Get the MCP client fot Composio MCP Server."""
+
+    client = MultiServerMCPClient(
+    {
+         "composio": {
+            # make sure you start your weather server on port 8000
+            "url": COMPOSIO_MCP_URL,
+            "transport": "http",
+            "headers": {
+                "x-api-key": COMPOSIO_API_KEY,
+            },
+        }
+    }
+    )
+    return client
+
+async def get_calendar_tools(client: MultiServerMCPClient):
+    """Get calendar tools from the Composio MCP Server."""
+    tools = await client.get_tools()
+    return [t for t in tools if "calendar" in t.name.lower()]
